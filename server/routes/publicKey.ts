cb69c97aa05c7c3bbfe31c7a9e7a33373a347f01
Kevin <kevin_mendez30@outlook.com>
2022-11-01 11:21:53 -0600

    feat: 🎸 (frontend) send public key to API

--- server/routes/publicKey.ts
+++ server/routes/publicKey.ts
@@ -4,5 +4,8 @@ export default (fastify: FastifyInstance, _: any, done: Function) => {
   fastify.get('/public-key', async (_: FastifyRequest, reply: FastifyReply) => {
     return { data: 'hola' }
   })
+  fastify.post('/public-key', async (request: FastifyRequest<{ Body: { public: string } }>, reply: FastifyReply) => {
+    return { data: request.body.public }
+  })
   done()
 }
