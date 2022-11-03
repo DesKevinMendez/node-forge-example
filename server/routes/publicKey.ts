556207d693c47ae5d849fdb9c61ba9c2f95bcfd7
Kevin <kevin_mendez30@outlook.com>
2022-11-03 14:40:38 -0600

    docs: ✏️ (README) for get .env files

--- server/routes/publicKey.ts
+++ server/routes/publicKey.ts
@@ -1,9 +1,13 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify'
+import { random, util } from 'node-forge';
 import { encriptAESkey } from '../utils/encript'
 
 export default (fastify: FastifyInstance, _: any, done: Function) => {
   fastify.get('/public-key', async (_: FastifyRequest, reply: FastifyReply) => {
-    return { data: 'hola' }
+    const key = random.getBytesSync(32);
+    let iv = random.getBytesSync(16);
+
+    return { BASE_64_KEY_ENCRIPT: util.bytesToHex(key), IV_ENCRIPT: util.bytesToHex(iv) }
   })
   fastify.post('/public-key', async (request: FastifyRequest<{ Body: { public: string } }>, reply: FastifyReply) => {
     const aesKeyEncripted = encriptAESkey(request.body.public)
